aa049f7b31fbfda14dd6a29db5a95eeb39e8355d
cryptodeal <39316528+cryptodeal@users.noreply.github.com>
2023-12-02 00:33:14 -0600

    day2 complete

--- src/day02.zig
+++ src/day02.zig
@@ -10,7 +10,57 @@ const gpa = util.gpa;
 
 const data = @embedFile("data/day02.txt");
 
-pub fn main() !void {}
+const GameData = struct {
+    red: usize = 0,
+    green: usize = 0,
+    blue: usize = 0,
+};
+
+const colors = [_][]const u8{ "red", "green", "blue" };
+
+fn parseGame(line: []const u8) !GameData {
+    var res = GameData{};
+    const start_idx = indexOf(u8, line, ':').? + 2;
+    var round_iterator = splitSeq(u8, line[start_idx..], "; ");
+    while (round_iterator.next()) |rnd| {
+        var color_iterator = splitSeq(u8, rnd, ", ");
+        while (color_iterator.next()) |pick| {
+            blk: inline for (colors) |color| {
+                if (std.mem.indexOf(u8, pick, color)) |idx| {
+                    const count = try parseInt(usize, pick[0 .. idx - 1], 10);
+                    if (@field(res, color) < count) {
+                        @field(res, color) = count;
+                    }
+                    break :blk;
+                }
+            }
+        }
+    }
+    return res;
+}
+
+pub fn main() !void {
+    var games = [_]GameData{undefined} ** 100;
+    var idx: usize = 0;
+    var iterator = splitSca(u8, data, '\n');
+    while (iterator.next()) |line| {
+        games[idx] = try parseGame(line);
+        idx += 1;
+    }
+    const pt1_red_limit: usize = 12;
+    const pt1_green_limit: usize = 13;
+    const pt1_blue_limit: usize = 14;
+    var sum_pt1: usize = 0;
+    var power_sum: usize = 0;
+    for (games, 0..) |g, i| {
+        power_sum += g.red * g.green * g.blue;
+        if (g.red <= pt1_red_limit and g.green <= pt1_green_limit and g.blue <= pt1_blue_limit) {
+            sum_pt1 += (i + 1);
+        }
+    }
+    std.debug.print("Part 1: {d}\n", .{sum_pt1});
+    std.debug.print("Part 2: {d}\n", .{power_sum});
+}
 
 // Useful stdlib functions
 const tokenizeAny = std.mem.tokenizeAny;
