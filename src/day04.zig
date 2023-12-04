56e33574e42579a64d1a3afd29018794c96bbfb9
cryptodeal <39316528+cryptodeal@users.noreply.github.com>
2023-12-04 01:23:34 -0600

    day4

--- src/day04.zig
+++ src/day04.zig
@@ -10,7 +10,71 @@ const gpa = util.gpa;
 
 const data = @embedFile("data/day04.txt");
 
-pub fn main() !void {}
+const CardInfo = struct {
+    winning_numbers: []usize,
+    numbers: []usize,
+};
+
+fn parseCard(allocator: std.mem.Allocator, line: []const u8) !CardInfo {
+    const idx = indexOf(u8, line, ':').? + 1;
+    var iter = splitSca(u8, line[idx..], '|');
+    const winning_numbers = iter.first();
+    var winners = std.ArrayList(usize).init(allocator);
+    var num_iter = splitSca(u8, winning_numbers, ' ');
+    while (num_iter.next()) |num| {
+        if (num.len == 0) continue;
+        try winners.append(try parseInt(usize, num, 10));
+    }
+    const numbers = iter.next().?;
+    var nums = std.ArrayList(usize).init(allocator);
+    num_iter = splitSca(u8, numbers, ' ');
+    while (num_iter.next()) |num| {
+        if (num.len == 0) continue;
+        try nums.append(try parseInt(usize, num, 10));
+    }
+    return .{
+        .winning_numbers = try winners.toOwnedSlice(),
+        .numbers = try nums.toOwnedSlice(),
+    };
+}
+
+fn calcMatches(info: CardInfo) usize {
+    var matches: usize = 0;
+    for (info.numbers) |num| {
+        if (indexOf(usize, info.winning_numbers, num)) |_| matches += 1;
+    }
+    return matches;
+}
+
+pub fn main() !void {
+    var cards = [_]CardInfo{undefined} ** 220;
+    var card_counts = [_]usize{1} ** 220;
+    defer for (cards) |c| {
+        gpa.free(c.winning_numbers);
+        gpa.free(c.numbers);
+    };
+    var iter = splitSca(u8, data, '\n');
+    var i: usize = 0;
+    while (iter.next()) |line| : (i += 1) {
+        cards[i] = try parseCard(gpa, line);
+    }
+    var pt1: usize = 0;
+    var pt2: usize = 0;
+    for (cards, 0..) |c, idx| {
+        pt2 += card_counts[idx];
+        const match_count = calcMatches(c);
+        var score: usize = 0;
+        for (0..match_count) |y| {
+            if (y == 1) score = 1 else score *= 2;
+        }
+        pt1 += score;
+        for (card_counts[idx + 1 .. idx + 1 + match_count]) |*count| {
+            count.* += 1 * card_counts[idx];
+        }
+    }
+    print("Part 1: {d}\n", .{pt1});
+    print("Part 2: {d}\n", .{pt2});
+}
 
 // Useful stdlib functions
 const tokenizeAny = std.mem.tokenizeAny;
